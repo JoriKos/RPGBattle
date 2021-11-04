ac8ba590581a1e99e76c0aa949fe3c7fdd05e062
DOOT <26204@ma-web.nl>
2021-11-04 20:22:28 +0100

    UI management for attack menu, healing menu start

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,7 +8,7 @@ public class UIManager : MonoBehaviour
     private string selector;
     private int selectedText;
     private int selectedMenu;
-    [SerializeField] private GameObject[] menuArray;
+    [SerializeField] private GameObject[] menuArray; //0 = options, 1 = attack, 2 = healing
     [SerializeField] private TextMeshProUGUI[] HealingMenuText; //0 = focused, 1 = wide
     [SerializeField] private TextMeshProUGUI[] AttackMenuText;  //0 = focused, 1 = wide
     [SerializeField] private TextMeshProUGUI[] OptionsMenuText; //0 = attack, 1 = heal, 2 = skip
@@ -18,6 +18,12 @@ public class UIManager : MonoBehaviour
         selector = "> ";
         selectedText = 0;
         selectedMenu = 0;
+
+        for (int i = 1; i < menuArray.Length - 1; i++)
+        {
+            menuArray[i].SetActive(false);
+        }
+
     }
 
     private void Update()
@@ -26,22 +32,23 @@ public class UIManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             selectedText++;
-            if (selectedText > 2)
-            {
-                selectedText = 0;
-            }
-            
-            if (selectedText < 0)
-            {
-                selectedText = 2;
-            }
 
             #region Options menu
-            if (selectedMenu == 0) //Options menu
+            if (selectedMenu == 0)
             {
-                OptionsMenuText[selectedText].text = selector + OptionsMenuText[selectedText].text;
-                Debug.Log(selectedText - 1);
+                if (selectedText > 2)
+                {
+                    selectedText = 0;
+                }
 
+                if (selectedText < 0)
+                {
+                    selectedText = 2;
+                }
+
+                OptionsMenuText[selectedText].text = selector + OptionsMenuText[selectedText].text; //Adds "> " to newly-selected menu text
+
+                //Removes previously selected menu text's selection indicator
                 if (selectedText - 1 == -1)
                 {
                     OptionsMenuText[selectedText + 2].text = OptionsMenuText[selectedText + 2].text.Remove(0, 2);
@@ -52,27 +59,55 @@ public class UIManager : MonoBehaviour
                 }
             }
             #endregion
+
+            #region Attack menu
+            if (selectedMenu == 1)
+            {
+                if (selectedText > 1)
+                {
+                    selectedText = 0;
+                }
+
+                if (selectedText < 0)
+                {
+                    selectedText = 1;
+                }
+
+                AttackMenuText[selectedText].text = selector + AttackMenuText[selectedText].text; //Adds "> " to newly-selected menu text
+
+                //Removes previously selected menu text's selection indicator
+                if (selectedText - 1 == -1)
+                {
+                    AttackMenuText[selectedText + 1].text = AttackMenuText[selectedText + 1].text.Remove(0, 2);
+                }
+                else
+                {
+                    AttackMenuText[selectedText - 1].text = AttackMenuText[selectedText - 1].text.Remove(0, 2);
+                }
+            }
+            #endregion
         }
 
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             selectedText--;
-            if (selectedText > 2)
-            {
-                selectedText = 0;
-            }
-            
-            if (selectedText < 0)
-            {
-                selectedText = 2;
-            }
 
             #region Options menu
-            if (selectedMenu == 0) //Options menu
+            if (selectedMenu == 0)
             {
-                OptionsMenuText[selectedText].text = selector + OptionsMenuText[selectedText].text;
-                Debug.Log(selectedText - 1);
+                if (selectedText > 2)
+                {
+                    selectedText = 0;
+                }
 
+                if (selectedText < 0)
+                {
+                    selectedText = 2;
+                }
+
+                OptionsMenuText[selectedText].text = selector + OptionsMenuText[selectedText].text; //Adds "> " to newly-selected menu text
+
+                //Removes previously selected menu text's selection indicator
                 if (selectedText + 1 == 3)
                 {
                     OptionsMenuText[selectedText - 2].text = OptionsMenuText[selectedText - 2].text.Remove(0, 2);
@@ -83,13 +118,47 @@ public class UIManager : MonoBehaviour
                 }
             }
             #endregion
+
+            #region Attack menu
+            if (selectedMenu == 1)
+            {
+                if (selectedText > 1)
+                {
+                    selectedText = 0;
+                }
+
+                if (selectedText < 0)
+                {
+                    selectedText = 1;
+                }
+
+                AttackMenuText[selectedText].text = selector + AttackMenuText[selectedText].text; //Adds "> " to newly-selected menu text
+
+                //Removes previously selected menu text's selection indicator
+                if (selectedText + 1 == 2)
+                {
+                    AttackMenuText[selectedText - 1].text = AttackMenuText[selectedText - 1].text.Remove(0, 2);
+                }
+                else
+                {
+                    AttackMenuText[selectedText + 1].text = AttackMenuText[selectedText + 1].text.Remove(0, 2);
+                }
+            }
+            #endregion
         }
 
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            if (selectedText == )
+            if (selectedText == 0)
             {
-
+                menuArray[1].SetActive(true);
+                selectedMenu = 1;
+            }
+            
+            if (selectedText == 1)
+            {
+                menuArray[2].SetActive(true);
+                selectedMenu = 2;
             }
         }
     }
